c79e49cc60a28a26a16e6f744242de5052b3f592
Jhonathan-Pizarra <jhonathanxabier1996@gmail.com>
2022-06-17 12:41:16 -0500

    Fomrs

--- next-serveless/pages/index.tsx
+++ next-serveless/pages/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next'
 import {Counter} from "../src/components/Counter";
 import {Usuario} from "../src/components/Usuario";
-
 import {TimerPadre} from "../src/components/TimerPadre";
 import {ContadorRed} from "../src/components/Reducer";
+import {Formulario} from "../src/components/Formulario";
 
 const Home: NextPage = () => {
   return (
@@ -19,6 +19,8 @@ const Home: NextPage = () => {
         <h2>UseReducer</h2>
         <ContadorRed/>
 
+        <h3>Hooks</h3>
+        <Formulario/>
     </div>
   )
 }
---
+++ next-serveless/src/components/Formulario.tsx
@@ -0,0 +1,39 @@
+import {ChangeEvent, useState} from "react";
+import {object} from "prop-types";
+
+//1) Creamos un formulario
+export const Formulario = () => {
+  const [formulario, setFormulario] = useState({
+      email: "",
+      nombre: ''
+  });
+
+  //2) Creamos nuestra funcion de cambio
+    const handleChange = ({target}: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = target;
+
+        setFormulario({
+            ...formulario,
+            [name] : value
+        })
+    }
+
+  return(
+      <div>
+         <form autoComplete="off">
+             <div>
+                 <label>Nombre:</label>
+                 <input onChange={handleChange} type="text" name="nombre"/>
+             </div>
+
+             <div>
+                 <label>Email:</label>
+                 <input type="email" name="email"/>
+             </div>
+
+             {/*Yo no puedo imprimir un objeto directamente en HTML, pero puedo serializarlo:*/}
+             <pre>{JSON.stringify(formulario)}</pre>
+         </form>
+      </div>
+  )
+}
